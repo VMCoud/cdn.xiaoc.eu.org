625407befbacf33fd5bed5d4e4a696085efcd767
i566 <58461724+i566@users.noreply.github.com>
2020-01-02 20:50:51 +0800

    Create cnzz.js

---
+++ cnzz.js
@@ -0,0 +1,3 @@
+var strVar = "";
+    strVar += "<div style=\"display:none\"><script type=\"text/javascript\">var cnzz_protocol = ((\"https:\" == document.location.protocol) ? \"https://\" : \"http://\");document.write(unescape(\"%3Cspan id='cnzz_stat_icon_1278547816'%3E%3C/span%3E%3Cscript src='\" + cnzz_protocol + \"s9.cnzz.com/z_stat.php%3Fid%3D1278547816' type='text/javascript'%3E%3C/script%3E\"));<\/script><\/div>\n";
+$("#header").append(strVar);
