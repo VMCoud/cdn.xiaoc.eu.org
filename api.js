937cbf5cb8b29e44927c375cfdd7c54a9db51ab8
i566 <58461724+i566@users.noreply.github.com>
2020-02-14 17:13:34 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,7 @@
-
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?2cd55d3fe80bb6cd7b1578ed9a979eb4";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
